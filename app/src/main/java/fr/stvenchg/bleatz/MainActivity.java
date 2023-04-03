9a474ce9259981bf8d88ea9575d0aba9f437e46e
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-04-03 13:39:47 +0200

    Fragment pour la navigation

--- app/src/main/java/fr/stvenchg/bleatz/MainActivity.java
+++ app/src/main/java/fr/stvenchg/bleatz/MainActivity.java
@@ -1,6 +1,9 @@
 package fr.stvenchg.bleatz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,21 +14,53 @@ import fr.stvenchg.bleatz.api.ApiClient;
 import fr.stvenchg.bleatz.api.ApiInterface;
 import fr.stvenchg.bleatz.api.AuthenticationManager;
 import fr.stvenchg.bleatz.api.account.AccountResponse;
+import fr.stvenchg.bleatz.databinding.ActivityMainBinding;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     private AuthenticationManager authenticationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.navigation.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.nav_item_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.nav_item_burgermenu:
+                    replaceFragment(new BurgerMenuFragment());
+                    break;
+                case R.id.nav_item_location:
+                    replaceFragment(new RestaurantLocationFragment());
+                    break;
+                case R.id.nav_item_account:
+                    replaceFragment(new AccountFragment());
+                    break;
+            }
+
+            return true;
+        });
 
         authenticationManager = new AuthenticationManager(this);
         checkEmailVerified();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_framelayout, fragment);
+        fragmentTransaction.commit();
     }
 
     private void checkEmailVerified() {
