d5341070512996e1a6d582d872bf3dcc0b94d876
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-04-03 03:03:51 +0200

    Ajout d'un splashscreen

--- app/src/main/java/fr/stvenchg/bleatz/AuthActivity.java
+++ app/src/main/java/fr/stvenchg/bleatz/AuthActivity.java
@@ -2,6 +2,7 @@ package fr.stvenchg.bleatz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,10 +30,19 @@ public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+        setContentView(R.layout.splashscreen);
 
-        refreshTokenAndRedirect();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_auth);
+                initializeViews();
+                refreshTokenAndRedirect();
+            }
+        }, 1500); // Affiche l'écran de démarrage pendant 5 secondes
+    }
 
+    private void initializeViews() {
         mLoginButton = findViewById(R.id.auth_button_login);
         mRegisterButton = findViewById(R.id.auth_button_register);
         mLaterTextView = findViewById(R.id.auth_textview_later);
