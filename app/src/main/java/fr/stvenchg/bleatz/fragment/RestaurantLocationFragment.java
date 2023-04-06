9cce26eb4060a74541295fe3c2eaa85ab0e29592
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-04-06 17:59:38 +0200

    Ajout de la carte Google

--- app/src/main/java/fr/stvenchg/bleatz/fragment/RestaurantLocationFragment.java
+++ app/src/main/java/fr/stvenchg/bleatz/fragment/RestaurantLocationFragment.java
@@ -1,66 +1,44 @@
 package fr.stvenchg.bleatz.fragment;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import fr.stvenchg.bleatz.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RestaurantLocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RestaurantLocationFragment extends Fragment {
+public class RestaurantLocationFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private GoogleMap mMap;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RestaurantLocationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantLocationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RestaurantLocationFragment newInstance(String param1, String param2) {
-        RestaurantLocationFragment fragment = new RestaurantLocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // TODO: Replace these coordinates with your restaurant's coordinates
+    private static final LatLng RESTAURANT_LOCATION = new LatLng(48.8566, 2.3522);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_restaurant_location, container, false);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker for the restaurant location and move the camera
+        mMap.addMarker(new MarkerOptions().position(RESTAURANT_LOCATION).title("Restaurant"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(RESTAURANT_LOCATION, 16));
     }
 }
