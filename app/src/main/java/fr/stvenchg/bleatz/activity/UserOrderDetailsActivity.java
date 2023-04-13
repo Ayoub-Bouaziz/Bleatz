f54ddcfec09b92d59f215a4c821350899751ef0c
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2023-04-13 16:14:40 +0200

    Démarrage création des vues detail commande

---
+++ app/src/main/java/fr/stvenchg/bleatz/activity/UserOrderDetailsActivity.java
@@ -0,0 +1,15 @@
+package fr.stvenchg.bleatz.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.stvenchg.bleatz.R;
+
+public class UserOrderDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordertrack);
+    }
+}
